000a86dc6123f4b681878c2ba5b6c0f32014c2c9
Maxim <maxkozhinov777@gmail.com>
2021-02-03 14:56:20 +0300

    Added Spring AOP support for logging

---
+++ src/main/java/com/maxkoz777/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,35 @@
+package com.maxkoz777.springdemo.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.maxkoz777.springdemo.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    @Pointcut("execution(* com.maxkoz777.springdemo.dao.*.*(..))")
+    private void forDAOPackage() {}
+
+    @Pointcut("execution(* com.maxkoz777.springdemo.service.*.*(..))")
+    private void forServicePackage() {}
+
+    @Pointcut("forDAOPackage() || forControllerPackage() || forServicePackage()")
+    private void forAppFlow() {}
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint) {
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("=====>>  in  @Before: calling method: " + method);
+    }
+
+}
